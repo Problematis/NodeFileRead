2a7e1a620ea82317a4cf72959c9e39722f84d207
Problematis <peter.hancock@selestaltd.co.uk>
2018-01-30 15:17:16 +0000

    2nd version

--- readfile.js
+++ readfile.js
@@ -4,3 +4,10 @@ fs.readFile('my-file.txt', 'utf8', function(err, data) {
     if (err) throw err;
     console.log(data);
 });
+
+try {  
+    var data = fs.readFileSync('my-file.txt', 'utf8');
+    console.log(data);    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
