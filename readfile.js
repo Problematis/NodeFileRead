d042563bfad964a504650ed8ad47ea32e5c2e90b
Problematis <peter.hancock@selestaltd.co.uk>
2018-01-30 16:12:01 +0000

    added copy

--- readfile.js
+++ readfile.js
@@ -1,9 +1,13 @@
 var fs = require('fs');
 
+// asynchronous read
+
 fs.readFile('my-file.txt', 'utf8', function(err, data) {  
     if (err) throw err;
     console.log(data);
 });
+
+// synchronous read
 
 try {  
     var data = fs.readFileSync('my-file.txt', 'utf8');
@@ -11,3 +15,12 @@ try {
 } catch(e) {
     console.log('Error:', e.stack);
 }
+
+// reading a file and creating a copy
+
+// copy a file
+
+const readableStream = fs.createReadStream('my-file.txt')
+var writableStream = fs.createWriteStream('my-file-copy.txt')
+
+readableStream.pipe(writableStream)
